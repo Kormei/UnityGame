92969ae7eaf7c887b99c96a7c7157f2e2c1cb711
JeffTheCodingCat <xk1045013@student.francistuttle.edu>
2025-04-16 09:26:28 -0500

    Camera yay
    added camera script that follows the player from a nonlinear distance also fixed the ball script (again)

--- Unity Game/Assets/Scripts/Ball.cs
+++ Unity Game/Assets/Scripts/Ball.cs
@@ -9,9 +9,9 @@ public class Ball : MonoBehaviour
         rigidbod = GetComponent<Rigidbody>();
     }
     // Impulse ball towards the center of the table considering player position
-    public void BallHit(Vector3 target, float strength)
+    public void BallHit(Vector3 directionFrom, Vector3 target, float strength)
     {
-        Vector3 direction = (target - transform.position).normalized;
+        Vector3 direction = (target - directionFrom).normalized;
         direction.y = .375f;
         rigidbod.velocity = Vector3.zero;
         rigidbod.AddForce(direction * strength, ForceMode.Impulse);
