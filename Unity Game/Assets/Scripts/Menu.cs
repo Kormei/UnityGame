bf80aef275d375730a9b555135c88dba6783e4c2
JeffTheCodingCat <xk1045013@student.francistuttle.edu>
2025-05-06 09:32:17 -0500

    titlecard
    its like invincible but instead of invincible its jimmy the ping pong wizard

---
+++ Unity Game/Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    public Canvas menu;
+    public Canvas settings;
+    public void OnStartButton()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+    public void OnSettings()
+    {
+        settings.gameObject.SetActive(true);
+        menu.gameObject.SetActive(false);
+    }
+    public void OnBack()
+    {
+        settings.gameObject.SetActive(false);
+        menu.gameObject.SetActive(true);
+    }
+}
